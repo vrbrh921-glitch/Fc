9498a20295d117590b7607c47d8605f9739cce11
vrbrh921-glitch <vrbrh921@gmail.com>
2026-01-09 17:11:43 +0530

    Update m.py

--- m.py
+++ m.py
@@ -74,8 +74,7 @@ class EndpointMonitor:
             time_int = 60  # Default to 60 seconds if conversion fails
             
         command = [
-            node", "y.js","GET", url, str(time_int), "4", "100" ,"--bfw" ,"--debug"
-        ]
+            "node", "y.js","GET", url, str(time_int), "4", "100" ,"--bfw" ,"--debug" ]
         
         logging.info(f"Method 1 - Executing: {' '.join(command)}")
         
